ce129bfe8a995709f7de5dca69b7374ad872b566
Laurent HUET <laurent.huet@gmail.com>
2014-03-05 00:03:10 +0100

    add first mongo aggregate request

--- app.js
+++ app.js
@@ -3,8 +3,10 @@
 var tarifbleu = require('tarifbleu');
 var loggerMongo = require('./mongo/mongologger');
 var consoinst = require('./teleinfo/consoinst');
+var mongo = require('./mongo/mongoHelpers');
 var express = require('express');
 var http = require('http');
+var util = require('util');
 
 var serialPort = process.env.TELE_PORT || '/dev/ttyAMA0';
 
@@ -34,19 +36,46 @@ app.get('/rest/inst/i', consoinst.iinst);
 app.get('/rest/inst/index', consoinst.index);
 
 // Requêtes mongo
-app.get('/rest/consojour/:jour', function(req, res) {
-/*    db.teleinfo.aggregate([
-    { $match : {datetime: {$gte: ISODate("2014-01-07T00:00:00Z")}}}, 
-    { $sort : { datetime: 1 } },
-    { $group : { 
-        _id : 1,
-        min : { $first: "$indexcpt"},
-        max : { $last: "$indexcpt"}
-        }
-    }
-    ]);*/
+app.get('/rest/testmongo', function(req, res) {
+    console.log('Requête HTTP mongo reçue')
+    var _db;
+    mongo.connect()
+       .then(function(db) {
+           var request = [
+               {$match: {datetime: {$gte:new Date("2014-01-01T00:00:00.000Z"), $lte:new Date("2014-02-17T00:00:00.000Z")}}},
+               {$project:
+               {annee: {'$year':'$datetime'},
+                   mois:{'$month':'$datetime'},
+                   jour:{'$dayOfMonth':'$datetime'},
+                   datetime:1,
+                   indexcpt:1}},
+               {$group:
+               {_id:{annee:'$annee', mois:'$mois', jour:'$jour'},
+                   mini:{$min:'$indexcpt'},
+                   maxi:{$max:'$indexcpt'}}},
+               {$project:
+               {conso:{$subtract:['$maxi', '$mini']},
+                   annee:{$substr: ["$_id.annee", 0, 4]},  // Workaround pour conversion d'un nombre en string
+                   mois:{$substr: ["$_id.mois", 0, 2]},
+                   jour:{$substr: ["$_id.jour", 0, 2]}}},
+               {$project:
+               {_id:0,
+                   date: {$concat: ['$annee', '-', '$mois', '-', '$jour']},
+                   conso: 1}},
+               {$sort:{conso:-1}}
+           ];
+           _db = db;
+           return mongo.aggregate(db, 'teleinfo', request);
+       })
+       .then(function(result) {
+           _db.close();
+           res.send(result);
+       })
+       .catch(function(err) {
+           console.log(err);
+            res.send(err);
+        });
 });
-
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Lancement application teleinfo sur le port ' + app.get('port'));
